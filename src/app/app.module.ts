d80f50e0c06e6c98a095a0b6dde97a578195dbf3
unknown <fady__malak@outlook.com>
2024-03-24 22:23:58 +0200

    generate empty components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,36 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/auth/login/login.component';
 import { RegisterComponent } from './components/auth/register/register.component';
+import { SidebarComponent } from './components/dashboard/sidebar/sidebar.component';
+import { BodyComponent } from './components/dashboard/body/body.component';
+import { NavbarComponent } from './components/frontend/navbar/navbar.component';
+import { FooterComponent } from './components/frontend/footer/footer.component';
+import { HomepageComponent } from './components/frontend/body/homepage/homepage.component';
+import { CartComponent } from './components/frontend/body/cart/cart.component';
+import { ProductDetailsComponent } from './components/frontend/body/product-details/product-details.component';
+import { SliderComponent } from './components/frontend/body/homepage/slider/slider.component';
+import { ProductsComponent } from './components/frontend/body/homepage/products/products.component';
+import { ClientReviewComponent } from './components/frontend/body/homepage/client-review/client-review.component';
+import { ProductItemComponent } from './components/frontend/body/homepage/products/product-item/product-item.component';
+import { CartItemComponent } from './components/frontend/body/cart/cart-item/cart-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    SidebarComponent,
+    BodyComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomepageComponent,
+    CartComponent,
+    ProductDetailsComponent,
+    SliderComponent,
+    ProductsComponent,
+    ClientReviewComponent,
+    ProductItemComponent,
+    CartItemComponent
   ],
   imports: [
     BrowserModule,
